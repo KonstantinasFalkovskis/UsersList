86672d85898a8f36652242ef6ff752cf047c1262
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-18 16:13:46 +0200

    save and update methods has been created

--- src/main/java/com/example/demo/controllers/UsersController.java
+++ src/main/java/com/example/demo/controllers/UsersController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controllers;
 
 import com.example.demo.dao.UsersDao;
+import com.example.demo.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class UsersController {
@@ -19,10 +24,22 @@ public class UsersController {
     public String showListOfUsers(Model model, @RequestParam(defaultValue = "0") int page) {
             model.addAttribute("data", usersDao.findAll(new PageRequest(page, 6)));
             model.addAttribute("currentPage", page);
-        return "index";
+        return "return:/";
     }
 
+    @PostMapping("/save")
+    public String persist(User users) {
+        usersDao.save(users);
+        return "redirect:/";
+    }
 
+    @RequestMapping("/edit/{id}")
+    public String update(Integer id) {
+        usersDao.findById(id);
+        return "redirect:/";
+    }
+
+    
 
 
 }
