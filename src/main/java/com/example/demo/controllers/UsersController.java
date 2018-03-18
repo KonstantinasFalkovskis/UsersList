531b22770802eda24c96bd00b55ffb4805361059
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-18 15:55:34 +0200

    Get mapping created. List of users method done.

--- src/main/java/com/example/demo/controllers/UsersController.java
+++ src/main/java/com/example/demo/controllers/UsersController.java
@@ -1,7 +1,28 @@
 package com.example.demo.controllers;
 
+import com.example.demo.dao.UsersDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UsersController {
+
+    @Autowired
+    private UsersDao usersDao;
+
+    @GetMapping("/")
+    public String showListOfUsers(Model model, @RequestParam(defaultValue = "0") int page) {
+            model.addAttribute("data", usersDao.findAll(new PageRequest(page, 6)));
+            model.addAttribute("currentPage", page);
+        return "index";
+    }
+
+
+
+
 }
