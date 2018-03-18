5745c7ef3e29150fb41be0390421b6cde403a007
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-18 15:43:39 +0200

    Jpa repository has been created (UserRepository.class)

---
+++ src/main/java/com/example/demo/dao/UsersDao.java
@@ -0,0 +1,11 @@
+
+package com.example.demo.dao;
+
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersDao extends JpaRepository<User, Integer> {
+
+}
