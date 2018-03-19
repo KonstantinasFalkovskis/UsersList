ae254e0f28c1958594f00a82f54511df9e2489af
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-19 16:10:50 +0200

    @Repository annotation +

--- src/main/java/com/example/demo/dao/UsersDao.java
+++ src/main/java/com/example/demo/dao/UsersDao.java
@@ -11,6 +11,7 @@ import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsersDao extends JpaRepository<User, Integer> {
 
 }
