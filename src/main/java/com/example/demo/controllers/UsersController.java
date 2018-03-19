dfeab9eac891dd4cc000ea647fd3a9ad3dc5fc65
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-19 16:09:50 +0200

    some commites was added

--- src/main/java/com/example/demo/controllers/UsersController.java
+++ src/main/java/com/example/demo/controllers/UsersController.java
@@ -21,9 +21,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UsersController {
 
+    //suite to the repository Jpa;
     @Autowired
     private UsersDao usersDao;
 
+    //method for users list viewing + pagination. Default is 6 items per page;
     @GetMapping("/")
     public String showListOfUsers(Model model, @RequestParam(defaultValue = "0") int page) {
             model.addAttribute("data", usersDao.findAll(new PageRequest(page, 6)));
@@ -31,18 +33,21 @@ public class UsersController {
         return "return:/";
     }
 
+    //method persist() for save item to Jpa virtual data base;
     @PostMapping("/save")
     public String persist(User users) {
         usersDao.save(users);
         return "redirect:/";
     }
 
+    //method update for find & edit item by item Id;
     @RequestMapping("/edit/{id}")
     public String update(Integer id) {
         usersDao.findById(id);
         return "redirect:/";
     }
 
+    //methode delete for removing item from Jpa data base. Item finding implements also by item id;
     @RequestMapping("/delete/{id}")
     public String delete(Integer id) {
         usersDao.deleteById(id);
--- src/main/java/com/example/demo/dao/UsersDao.java
+++ src/main/java/com/example/demo/dao/UsersDao.java
@@ -11,7 +11,6 @@ import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UsersDao extends JpaRepository<User, Integer> {
 
 }
