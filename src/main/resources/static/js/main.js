13180fda20d2268bc6d2a21f8c6be709396c54c9
Konstantinas Falkovskis <integro@inbox.lt>
2018-03-20 11:14:03 +0200

    some commites added

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,17 @@
-
+/**
+ * @Author Falco Constantine
+ * @Project Users list
+ * @Version 1.0
+ * @Date 2018.03.20
+ *
+ */
 $(document).ready(function(){
+
     $('.nBtn, .table .eBtn').on('click', function(event) {
         event.preventDefault();
         var href = $(this).attr('href');
         var text = $(this).text();
-
+        //for update user
         if (text == 'Edit') {
             $.get(href, function (users, status) {
                 $('.myFormUpdate #id').val(users.id);
@@ -14,6 +21,7 @@ $(document).ready(function(){
             });
             $('.myFormUpdate #updateModal').modal();
         } else {
+            //for creating user
             // $('.myFormCreate #id').val('');
             $('.myFormCreate #username').val('');
             $('.myFormCreate #password').val('');
@@ -21,6 +29,7 @@ $(document).ready(function(){
             $('.myFormCreate #myModalCreate').modal();
         }
     });
+    //for delete user
     $('.table .delBtn').on('click', function(event) {
         event.preventDefault();
         var href = $(this).attr('href');
